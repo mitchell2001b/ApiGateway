3e0fee2c1e81905d96e43aac958868fa25786291
mitchell2001b <79633852+mitchell2001b@users.noreply.github.com>
2023-12-06 15:54:01 +0100

    new routes ssssssss

--- src/main/java/com/example/ApiGateway/ApiGatewayApplication.java
+++ src/main/java/com/example/ApiGateway/ApiGatewayApplication.java
@@ -1,36 +1,13 @@
 package com.example.ApiGateway;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import io.github.cdimascio.dotenv.DotenvEntry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.core.env.MapPropertySource;
-import org.springframework.core.env.MutablePropertySources;
-import org.springframework.core.env.StandardEnvironment;
-
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
 
-
 	public static void main(String[] args) {
-
-		Map<String, Object> env = Dotenv.load()
-				.entries()
-				.stream()
-				.collect(
-						Collectors.toMap(DotenvEntry::getKey, DotenvEntry::getValue));
-		new SpringApplicationBuilder(ApiGatewayApplication.class)
-				.environment(new StandardEnvironment() {
-					@Override
-					protected void customizePropertySources(MutablePropertySources propertySources) {
-						super.customizePropertySources(propertySources);
-						propertySources.addLast(new MapPropertySource(".env", env));
-					}
-				}).run(args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
